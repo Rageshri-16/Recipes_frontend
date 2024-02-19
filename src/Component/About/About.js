5767a35a5dfd7f49f968cec1be02df9b041907a9
bharti <bharti77@gmail.com>
2024-02-19 10:09:01 +0530

    first cimmit

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -21,7 +21,7 @@ function About() {
       </div>
       <div className="card text-center m-2" style={{ backgroundColor: "#bddaf2" }}>
         <div className="card-header">
-          <h1>about AS </h1>
+          <h1>service  </h1>
         </div>
         <div className="card-body">
           <div className="container my-2">
